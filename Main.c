73efd3376f3ff8cda507cb32b9d45de66b28f966
KavinduJayas <59658804+KavinduJayas@users.noreply.github.com>
2020-05-31 00:17:58 +0530

    Update Main.c

--- Main.c
+++ Main.c
@@ -1,6 +1,40 @@
 #include <stdio.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct _{
+	char word[50];
+	float frequency;
+}wordFrequency_t;
+
+typedef struct __{
+	char letter;
+	float frequency;
+}letterFrequency_t;
+
+FILE* readFile(char*);
+float countLetter(char);
+float countWord(char*);
+void printWordGraph(wordFrequency_t*);
+void printWordGraph(letterFrequency_t*);
 
 int main(){
 
+
 	return 0;
 }
+
+FILE* readFile(char* file){
+	FILE *fp;
+
+	fp = fopen(file, "r"); // read mode
+
+	if (fp == NULL)
+	{
+		perror("Error while opening the file.\n");
+		exit(EXIT_FAILURE);
+	}
+
+} 
+
