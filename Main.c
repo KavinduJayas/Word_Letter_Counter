49e0a8bbc7191e555343c5d5e310aaba6f71572b
KavinduJayas <59658804+KavinduJayas@users.noreply.github.com>
2020-05-31 23:48:16 +0530

    Update Main.c

--- Main.c
+++ Main.c
@@ -13,19 +13,33 @@ typedef struct __{
 	float frequency;
 }letterFrequency_t;
 
-FILE* readFile(char*);
-float countLetter(char);
-float countWord(char*);
+FILE* openFile(char*);
+int fileSize(FILE*);
+char* preProcessor(char*);
+letterFrequency_t countLetter(char*,char);
+wordFrequency_t countWord(char*,char*);
 void printWordGraph(wordFrequency_t*);
 void printLetterGraph(letterFrequency_t*);
 
 int main(int argc, char* argv[]){
+	char* allString =NULL;	
+	FILE* fp=NULL;
+	wordFrequency_t* wordFrequency = (wordFrequency_t*)malloc(sizeof(wordFrequency_t)*10);
+	letterFrequency_t* letterFrequency = (letterFrequency_t*)malloc(sizeof(letterFrequency_t)*10);
+
+	fp = openFile(argv[3]);
+
+	allString = (char*)malloc(fileSize(fp));
+	allString = preProcessor(allString);
 	
+	wordFrequency[i]=
+	printWordGraph(wordFrequency);
+	printLetterGraph(letterFrequency);
 
 	return 0;
 }
 
-FILE* readFile(char* file){
+FILE* openFile(char* file){
 	FILE *fp;
 
 	fp = fopen(file, "r"); // read mode
@@ -38,3 +52,7 @@ FILE* readFile(char* file){
 
 } 
 
+int fileSize(FILE* fp){
+	fseek(fp, 0, SEEK_END);
+	return ftell(fp);    
+}
